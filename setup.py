c0ea9814d45d8dcfed756ce4e3b619f9d85ce5f7
Ben Avrahami <sample@notreal.fake>
2019-11-11 15:54:16 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 import containerview_data as containerview
 
 setuptools.setup(
-    name='snake_eyes',
+    name='containerview',
     version=containerview.__version__,
     url=containerview.__url__,
     author=containerview.__author__,
-    packages=['sequenceview', 'sequenceview_data'],
+    packages=['containerview', 'containerview_data'],
     python_requires='>=3.7.0',
     include_package_data=True,
     data_files=[
